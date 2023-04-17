835535319ec5df72c56af81999737c2131630552
masharqah <59175914+masharqah@users.noreply.github.com>
2023-04-17 17:55:29 +0300

    Assignment

---
+++ Loop_challenges.js
@@ -0,0 +1,30 @@
+// //Print odds 1-20
+
+for(i=1; i<=20;i++)
+    if(i%2==1)
+    {console.log(i);}
+
+// //Decreasing Multiples of 3
+
+for(i=100;i>=0;i--)
+    if(i%3==0)
+    {console.log(i);}
+
+// //Print the sequence -1.5 increment
+for (i=4;i>=-3.5;i-=1.5){
+    console.log(i);
+}
+
+//Sigma
+var count=0;
+for(i=0;i<=100;i++)
+ {count+=i;}
+console.log(count);
+
+//Factorial 
+var count=1;
+for(i=1;i<=12;i++)
+ {count*=i;}
+console.log(count);
+
+
